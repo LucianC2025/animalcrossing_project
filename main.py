5144da8dd322a228817c633f5004d7e16b02bf5d
LucianC2025 <lucian.cruz2025@gbwl.org>
2025-04-02 12:30:41 +0000

    added comments / game plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("hello world")
-
-
-if __name__ == "__main__":
-    main()
+# ********* IDEAS *********
+# 1. create a catalog out of the catalog csv files
+# 2. create an area for "favorites"
+    # favorite items
+    # favorite DIYS
+    # favorite characters
+# 3. create a "completion" area 
